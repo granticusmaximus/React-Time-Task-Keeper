e74b329e6de44ea1d2e1ad92f019345802ca034e
Grant Watson <gwatson117@gmail.com>
2020-01-27 14:54:20 -0700

    initial commit

---
+++ src/config/dev.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyAaQFrj08PsXwS4I0yRxJm4fQTPXFnsk2g",
+    authDomain: "timetracker-f6909.firebaseapp.com",
+    databaseURL: "https://timetracker-f6909.firebaseio.com",
+    projectId: "timetracker-f6909",
+    storageBucket: "timetracker-f6909.appspot.com",
+    messagingSenderId: "959462901051",
+  }
